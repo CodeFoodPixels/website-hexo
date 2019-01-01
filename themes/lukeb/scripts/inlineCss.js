03735e62dfe771267e4b629dec1eb961004fd7ac
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-01-01 22:17:52 +0000

    Change path for import

--- themes/lukeb/scripts/inlineCss.js
+++ themes/lukeb/scripts/inlineCss.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('hexo-fs');
 const path = require('path');
 const cheerio = require('cheerio');
 const csso = require('csso');
@@ -7,7 +7,7 @@ hexo.extend.filter.register('after_render:html', function (str) {
     const $ = cheerio.load(str);
 
     $('link[critical="true"]').each(function() {
-        const content = csso.minify(fs.readFileSync(path.join('public', $(this).attr('href')))).css;
+        const content = csso.minify(fs.readFileSync(path.join('themes', hexo.config.theme, 'source', $(this).attr('href')))).css;
 
         $(this).replaceWith(`<style>${content}</style>`);
     });
