bd3321e1a39b06381e49dbf6bc4f69fc21752a38
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-01-16 22:59:06 +0000

    Fix incorrect path for background

--- themes/lukeb/scripts/inlineCss.js
+++ themes/lukeb/scripts/inlineCss.js
@@ -1,13 +1,12 @@
-const fs = require('hexo-fs');
+const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
-const csso = require('csso');
 
 hexo.extend.filter.register('after_render:html', function (str) {
     const $ = cheerio.load(str);
 
     $('link[critical="true"]').each(function() {
-        const content = csso.minify(fs.readFileSync(path.join('themes', hexo.config.theme, 'source', $(this).attr('href')))).css;
+        const content = fs.readFileSync(path.join('themes', hexo.config.theme, 'source', $(this).attr('href')));
 
         $(this).replaceWith(`<style>${content}</style>`);
     });
